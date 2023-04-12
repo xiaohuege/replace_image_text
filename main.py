adc97de70e890a8736a292bf5a92072a06d6f7fa
icewater506 <506896791@qq.com>
2023-04-12 14:35:04 +0800

    增加测试图片

--- main.py
+++ main.py
@@ -59,5 +59,5 @@ def bootatrap(imgPath):
   image.save(imgPath.replace('/data/', '/output/'))
 
 if __name__ == '__main__':
-  imgPath = path.realpath('./data/16.jpeg')
+  imgPath = path.realpath('./data/20.jpeg')
   bootatrap(imgPath)
